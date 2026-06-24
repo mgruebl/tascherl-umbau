f8cef84079102ff464a3c24df4575293a9d586d4
Roman-Tascherl <rrafetseder@student.tgm.ac.at>
2026-06-24 09:34:31 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -51,7 +51,7 @@ const initialCards = [
     company: "Amazon",
     brand: "amazon",
     type: "QR",
-    category: "Test",
+    category: "TEST2222222",
     color: "from-slate-400 to-black",
     info: "Online Account",
     code: "AMZ-22341",
